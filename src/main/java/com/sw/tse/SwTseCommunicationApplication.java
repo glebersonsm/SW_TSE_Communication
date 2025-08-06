1cd6b091a6e4df58d126d86fa90281cd333fd5ee
smunaretto <32823933+smunaretto@users.noreply.github.com>
2025-08-05 21:11:06 -0300

    Commit inicial

---
+++ src/main/java/com/sw/tse/SwTseCommunicationApplication.java
@@ -0,0 +1,13 @@
+package com.sw.tse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwTseCommunicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwTseCommunicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sw/tse/SwTseCommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw.tse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwTseCommunicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
